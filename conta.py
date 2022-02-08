807b6f110d32a4ab17b9091ca35bc54679c93da0
Vinicius <viniaasouza98@gmail.com>
2022-02-08 19:52:55 -0300

    atualizações de arquivos

--- conta.py
+++ conta.py
@@ -1,8 +1,17 @@
 
-class Conta:
+class Conta:#classe
     def __init__(self, numero, titular, saldo, limite):#função construtora
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.numero = numero#objeto
+        self.titular = titular#objeto
+        self.saldo = saldo#objeto
+        self.limite = limite#objeto
+
+    def extrato(self):#metodo
+        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
