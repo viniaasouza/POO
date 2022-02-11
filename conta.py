87a319d96478b458af9e5c0bef5c739641d6c9ae
Vinicius <viniaasouza98@gmail.com>
2022-02-11 00:34:25 -0300

    "atualização aula POO"

--- conta.py
+++ conta.py
@@ -2,16 +2,43 @@
 class Conta:#classe
     def __init__(self, numero, titular, saldo, limite):#função construtora
         print("Construindo objeto...{}".format(self))
-        self.numero = numero#objeto
-        self.titular = titular#objeto
-        self.saldo = saldo#objeto
-        self.limite = limite#objeto
+        self.__numero = numero#objeto # o "__" deixa o atributo privado
+        self.__titular = titular#objeto
+        self.__saldo = saldo#objeto
+        self.__limite = limite#objeto
 
     def extrato(self):#metodo
-        print("Saldo {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_saque = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_saque
 
     def saca(self, valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.saca(valor)
+
+    @property #Criação de um getter. getter sempre tem que ter um retorno
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter #Criação de um setter.
+    def limite(self, limite):
+        self.__limite = limite
